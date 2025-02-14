540186c5e483a1c0456eeaf4f6a4a2deff450fbc
Ciaran McCormack <ciaran@raiblin.com>
2025-02-15 09:54:10 +1100

    Various project changes to get the default project to work on github pages and on custom domain https://proceduralplanet.com

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/ProceduralPlanet/' : '/',
+  base: command === 'build' ? '/' : '/',
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/CNAME', // Copy CNAME from public/
+          dest: '.'           // Place it in dist/
+        }
+      ]
+    })
+  ]
 }));
